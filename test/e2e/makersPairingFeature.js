07db3efdb2160025bf3550fa117bedcd74795335
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-14 19:53:01 +0100

    Protractor Stubbing switched to protractor-http-mock

--- test/e2e/makersPairingFeature.js
+++ test/e2e/makersPairingFeature.js
@@ -1,16 +1,32 @@
+var mock = require('protractor-http-mock');
 describe('Makers Pairing Finder', function() {
+
   var addMakerBox = element(by.model('addMakerCtrl.addMaker'));
   var addMakerButton = element(by.className('btn'));
 
-  beforeEach(function() {
-    browser.get('http://localhost:3030');
+  afterEach(function(){
+    mock.teardown();
   });
 
   it('has a title', function() {
+    browser.get('http://localhost:3000');
     expect(browser.getTitle()).toEqual('Makers Pairing Finder');
   });
 
   it('can add a Maker', function() {
+    mock([{
+      request: {
+        path: '/makers',
+        method: 'POST'
+      },
+      response: {
+        data: {
+          id: 1,
+          name: 'Joe'
+        }
+      }
+    }]);
+    browser.get('http://localhost:3000');
     addMakerBox.sendKeys('Joe');
     addMakerButton.click();
     expect(element(by.css('.notice'))).toMatch('Joe added');
