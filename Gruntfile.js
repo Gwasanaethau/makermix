c3c35d1bc994508dbeff549263b07475a4efbebe
Mark Pokorny <iridium193@gmail.com>
2015-05-17 00:04:10 +0100

    Added ‘Gruntfile.js’ and npm modules in preparation for travis integration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    pkg:grunt.file.readJSON('package.json'),
     ngconstant:{
       options:{
         name:'config',
@@ -22,9 +21,75 @@ module.exports = function(grunt){
           }
         }
       }
+    },
+    jshint:{
+      all:[
+        'Gruntfile.js',
+        'test/**/*.js',
+        'public/js/*.js',
+        'server.js',
+        'karma.conf.js'
+      ]
+    },
+    karma:{
+      unit:{
+        configFile:'karma.conf.js',
+        singleRun:true,
+        browsers:['PhantomJS']
+      }
+    },
+    exec:{
+      updateWebdriver:{
+        command:'node_modules/webdriver-manager/bin/webdriver-manager update --standalone'
+      }
+    },
+    express:{
+      dev:{
+        options:{
+          script:'server.js'
+        }
+      }
+    },
+    protractor_webdriver:{
+      options:{
+        path:'node_modules/webdriver-manager/bin/',
+        command:'webdriver-manager start'
+      },
+      all:{
+        options:{}
+      }
+    },
+    apimocker:{
+      options:{
+        configFile:'apimocker-grunt.json'
+      }
+    },
+    protractor:{
+      options:{
+        configFile:'test/e2e/conf.js',
+        args:{
+          capabilities:{
+            'browserName':'phantomjs',
+            'phantomjs.binary.path':require('phantomjs').path,
+            'phantomjs.ghostdriver.cli.args':['--loglevel=DEBUG']
+          }
+        }
+      },
+      all:{
+        options:{
+          configFile:'test/e2e/conf.js'
+        }
+      }
     }
   });
   grunt.loadNpmTasks('grunt-ng-constant');
-  grunt.registerTask('default', ['ngconstant:testing']);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-protractor-webdriver');
+  grunt.loadNpmTasks('grunt-apimocker');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+  grunt.registerTask('default',['ngconstant:testing','jshint','karma','exec','express','apimocker','protractor_webdriver','protractor']);
   grunt.registerTask('deployment', ['ngconstant:production']);
 };
